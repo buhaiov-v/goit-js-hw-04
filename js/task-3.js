25892082b811c1bca09e07920b9ef563517c5df8
buhaiov-v <bugayov.v.s@gmail.com>
2024-12-22 18:58:48 +0200

    js dz4

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,21 @@
-function getElementWidth (content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+        this.username = newName;
+    },
+  updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
 
-    const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
+console.log(profile.getInfo()); 
 
-    return totalWidth;
-}
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
